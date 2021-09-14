ade0358f21642cd3cc0dd32ada756e82b55c740e
Chris <ChrisGavlon@gmail.com>
2021-09-14 12:51:57 -0500

    updated by Chris

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const BASE_URL =
-  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=500&page=1&sparkline=false";
+// const BASE_URL =
+//   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=500&page=1&sparkline=false";
+
+const BASE_URL = "http://localhost:3000/Crypto";
 
 fetch(BASE_URL)
   .then((resp) => {
