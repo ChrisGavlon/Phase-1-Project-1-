bbd0bb57a73d3d64d599133dc485f560f948d5ad
Chris <ChrisGavlon@gmail.com>
2021-09-15 16:38:14 -0500

    Optimized + Calc Ready

--- index.js
+++ index.js
@@ -10,27 +10,23 @@ fetch(BASE_URL)
 
   .then(function (cryptoArray) {
     cryptoArray.forEach((crypto) => {
-      renderCryptoDropDown(crypto);
-    });
-    cryptoArray.forEach((crypto) => {
-      renderCryptoDropDown2(crypto);
+      renderCryptoDropDown(crypto, "1", "1");
+      renderCryptoDropDown(crypto, "2", "2");
     });
 
-    function renderCryptoDropDown(crytpoObj) {
-      //   console.log(crytpoObj); // now we get the individual objects
-
+    function renderCryptoDropDown(crytpoObj, dropDownID, inputID) {
       // create option or div element where data will go
       const cryptoSearchCell = document.createElement("div");
-      //   cryptoSearchDiv.classList = "cryptoSearchCells";
+      // assign a div 
       cryptoSearchCell.textContent = crytpoObj.name;
 
-      cryptoSearchCell.id = crytpoObj.id;
+      cryptoSearchCell.id = inputID;
       console.log(cryptoSearchCell.id);
-
+      debugger;
 
       cryptoSearchCell.addEventListener("click", insertToSearchbar);
-
-      const searchCellLocation = document.querySelector("#myDropdown");
+      
+      const searchCellLocation = document.querySelector(`#myDropDown${dropDownID}`);
 
       // const imageSearchCell = document.createElement("img");
       // imageSearchCell.src = crytpoObj.image;
@@ -39,55 +35,57 @@ fetch(BASE_URL)
 
       searchCellLocation.appendChild(cryptoSearchCell);
       // cryptoSearchCell.appendChild(imageSearchCell);
+
+      // dataset variables for Marketcap, Price, and Total Supply
+
+      let dataPrice = document.querySelector(`#myInput${inputID}`);
+      dataPrice.dataset.price = crytpoObj.price;
     }
-
-
-function renderCryptoDropDown2(crytpoObj) {
-  //   console.log(crytpoObj); // now we get the individual objects
-
-  // create option or div element where data will go
-    const cryptoSearchCell = document.createElement("div");
-  //   cryptoSearchDiv.classList = "cryptoSearchCells";
-    cryptoSearchCell.textContent = crytpoObj.name;
-
-    cryptoSearchCell.id = crytpoObj.id;
-    console.log(cryptoSearchCell.id);
-
-
-    cryptoSearchCell.addEventListener("click", insertToSearchbar2);
-
-    const searchCellLocation = document.querySelector("#myDropdown2");
-
-    // const imageSearchCell = document.createElement("img");
-    // imageSearchCell.src = crytpoObj.image;
-
-    // append cryptoSearchCell to SearchCellLocation
-
-    searchCellLocation.appendChild(cryptoSearchCell);
-    // cryptoSearchCell.appendChild(imageSearchCell);
-  }
   });
 
-function insertToSearchbar2(e) {
-  const input = document.querySelector("#myInput2");
-  input.value = this.outerText;
-}
+fetch(BASE_URL)
+  .then((resp) => {
+    return resp.json()
+  })
+  .then((cryptoArray)=>{cryptoArray.forEach((crypto) =>{
+      getInputValues(crypto, "1")
+      getInputValues(crypto, "2")
+    })
+  })
+
+  function getInputValues(cryptoObj, inputID){
+    let dataPrice = document.querySelector(`#myInput${inputID}`);
+    dataPrice.dataset.price = cryptoObj.price;
+
+    let marketCap = document.querySelector(`#myInput${inputID}`)
+    marketCap.dataset.mCap = cryptoObj.market_cap
+
+    let circSupply = document.querySelector(`#myInput${inputID}`)
+    circSupply.dataset.cSupply = cryptoObj.circulating_supply
+  }
 
 function insertToSearchbar(e) {
-const input = document.querySelector("#myInput");
+const input = document.querySelector(`#myInput${e.target.id}`);
 input.value = this.outerText;
 }
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+// const inputTOCalc1 = document.querySelector("#myInput1");
+// const inputTOCalc2 = document.querySelector("#myInput2");
 
-function filterFunction() {
-  let input, filter, ul, li, div, i;
+// inputTOCalc1.addEventListener("change", function () {
+//   return console.log(inputTOCalc1.value);
+// });
+// inputTOCalc2.addEventListener("change", function () {
+//   return console.log(inputTOCalc2.value);
+// });
 
-  input = document.getElementById("myInput");
+function filterFunction(numID) {
+  let input, filter, div, i;
+
+  input = document.querySelector(`#myInput${numID}`);
   filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
-  div = div.getElementsByTagName("div");
+  div = document.querySelector(`#myDropdown${numID}`);
+  div = document.getElementsByTagName("div");
 
   for (i = 0; i < div.length; i++) {
     txtValue = div[i].textContent || div[i].innerText;
@@ -99,20 +97,23 @@ function filterFunction() {
   }
 }
 
-function filterFunction2() {
-  let input, filter, ul, li, div, i;
 
-  input = document.getElementById("myInput2");
-  filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown2");
-  div = div.getElementsByTagName("div");
+//----------------------------Calculation Portion -------------------
 
-  for (i = 0; i < div.length; i++) {
-    txtValue = div[i].textContent || div[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      div[i].style.display = "";
-    } else {
-      div[i].style.display = "none";
-    }
-  }
-}
+// event listners attached to input field of each from, which has a function embedded into it that retrieves the value of each
+
+// function getVal() {
+//   const inputVal1 = document.querySelector("#myInput").value;
+//   const inputVal2 = document.querySelector("#myInput2").value;
+
+//   console.log(inputVal1);
+//   console.log(inputVal2);
+// }
+
+/// calculation requires grabbing two values from each input -i.e, the crypto we have chosen.
+
+// need to grab B's market cap and divide it by A's suplly to create new variable called newMultiple, which we then multiple it by the current price of A. This will result in the NewPrice.
+
+// inputBMarketCap / inputASupply = newMultiple
+
+//newMultiple x inputACurrentPrice = newPrice, which will be appended to the price container
